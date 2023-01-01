8a2688d7b99952878c0005fc149ebb1d3b712055
HARITS <harits.muhammad.only@gmail.com>
2023-01-01 11:00:59 +0700

    successfully get detail url with rotate proxy, build get datas function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+import os
 import random
 
 import requests
@@ -60,7 +62,6 @@ def get_detail_url(url, selected_proxies):
     next_page_url = url
     detail_urls = list()
     while not end:
-        print(next_page_url)
         if page % 10 == 0:
             selected_proxy = random.choice(selected_proxies)
         trial = 0
@@ -75,7 +76,7 @@ def get_detail_url(url, selected_proxies):
                     'referer': next_page_url,
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
                     }
-                response = session.get(next_page_url, proxies=formated_proxy, headers=headers, timeout=3)
+                response = session.get(next_page_url, proxies=formated_proxy, headers=headers, timeout=(3,27))
                 session.close()
                 break
             except (ProxyError, ConnectTimeout) as e:
@@ -86,16 +87,17 @@ def get_detail_url(url, selected_proxies):
 
         if trial < 8:
             soup = BeautifulSoup(response.text, 'html.parser')
-            next_page_url = f"{url_schema}{soup.select_one(next_page_locator)['href']}"
+            try:
+                next_page_url = f"{url_schema}{soup.select_one(next_page_locator)['href']}"
+            except TypeError:
+                end = True
             html_detail_urls = soup.select(detail_url_locators)
             url_locator = "div.cy5jw6o.dir.dir-ltr > a"
             for i in html_detail_urls:
                 detail_urls.append(f"{url_schema}/{i.select_one(url_locator)['href']}")
-
             print(f"{len(detail_urls)} url collected from {str(page)} page")
             page += 1
-            if next_page_url == None:
-                end = True
+
         else:
             print("Proxy Error")
             detail_urls = None
@@ -103,15 +105,81 @@ def get_detail_url(url, selected_proxies):
 
     return detail_urls
 
-def to_file(data, save_path):
-    pass
+def get_datas(urls, selected_proxies):
+    print('Collecting datas...')
+    datas = list()
+    name_locator = 'h1._fecoyn4'
+    instagram_locator = 'div.d1isfkwk.dir.dir-ltr>span.ll4r2nl.dir.dir-ltr'
+    website_locator = 'div.d1isfkwk.dir.dir-ltr>span.ll4r2nl.dir.dir-ltr'
+
+    for counter, url in enumerate(urls):
+        if counter % 10:
+            selected_proxy = random.choice(selected_proxies)
+        data = {
+            'link': [],
+            'name': [],
+            'instagram': [],
+            'website': []
+        }
+        trial = 0
+        while (trial < 3):
+            try:
+                session = requests.Session()
+                formated_proxy = {
+                    "http": f"http://{selected_proxy}",
+                    "https": f"http://{selected_proxy}"
+                }
+                headers = {
+                    'referer': url,
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
+                    }
+                response = session.get(url, proxies=formated_proxy, headers=headers, timeout=(3,27))
+                session.close()
+                break
+            except (ProxyError, ConnectTimeout) as e:
+                print(e)
+                selected_proxy = random.choice(selected_proxies)
+                print(f"Change proxy to {selected_proxy}")
+                trial += 1
+
+        if trial < 3:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            try:
+                data['link'] = response.url()
+                data['name'] = soup.select_one(name_locator)
+                data['instagram'] = soup.select_one(instagram_locator)
+                data['website'] = soup.select_one(website_locator)
+            except TypeError:
+                break
+            datas.append(data.copy())
+
+        else:
+            continue
+
+    return datas
+
+def to_csv(datas=None, filepath=None):
+    print('Creating file...')
+    folder = filepath.rsplit("/", 1)[0]
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+    with open(filepath, 'w+', encoding="utf-8", newline='') as f:
+        headers = ['link', 'name', 'instagram', 'website']
+        writer = csv.DictWriter(f, fieldnames=headers)
+        writer.writeheader()
+        for i in datas:
+            writer.writerow(i)
+        f.close()
+    print(f'{filepath} created')
 
 if __name__ == '__main__':
     url = "https://www.airbnb.com/s/Belgium/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&price_filter_input_type=0&price_filter_num_nights=5&query=Belgium&place_id=ChIJl5fz7WR9wUcR8g_mObTy60c&date_picker_type=calendar&flexible_trip_lengths%5B%5D=weekend_trip&checkin=2023-01-29&checkout=2023-01-30&adults=1&source=structured_search_input_header&search_type=autocomplete_click"
-    save_path = "C:\project\airbnbscraper\result.csv"
-    # proxies = get_proxy()
-    # selected_proxies = choose_proxy(proxies)
-    selected_proxies = ["50.238.158.12:8080", "134.238.252.143:8080"]
-    detail_urls = get_detail_url(url,selected_proxies=selected_proxies)
+    save_path = r"C:\project\airbnbscraper\result.csv"
+    proxies = get_proxy()
+    selected_proxies = choose_proxy(proxies)
+    detail_urls = get_detail_url(url, selected_proxies=selected_proxies)
+    datas = get_datas(detail_urls, selected_proxies=selected_proxies)
 
-    # to_file(data, save_path)
+    to_csv(datas, save_path)
