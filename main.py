ca2642d5613eba8e94c9b866c8da3da36d881f6a
HARITS <harits.muhammad.only@gmail.com>
2023-01-05 05:39:48 +0700

    try to get instagram get none from select_one

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import random
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -45,10 +46,12 @@ def choose_proxy(proxies):
             print(f'checking {formated_proxy}')
             try:
                 with requests.Session() as session:
-                    session.get(url='https://www.airbnb.com', proxies=formated_proxy, timeout=3)
-                session.close()
-                selected_proxies.append(item)
-                print(f'{item} selected')
+                    response = session.get(url='https://www.google.com', proxies=formated_proxy, timeout=5)
+                if response.status_code == 200:
+                    selected_proxies.append(item)
+                    print(f'{item} selected')
+                else:
+                    print(f"not working with status code{response.status_code}")
             except Exception as e:
                 print(f"not working with {e}")
                 pass
@@ -84,7 +87,8 @@ def get_detail_url(url, selected_proxies):
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
                     }
-                response = session.get(next_page_url, proxies=formated_proxy, headers=headers, timeout=(3,27))
+                response = session.get(next_page_url, proxies=formated_proxy, headers=headers, timeout=(5,27))
+                time.sleep(5)
                 session.close()
                 success = True
             except (ProxyError, ConnectTimeout, ReadTimeoutError, ReadTimeout, ConnectionError, ConnectionError) as e:
@@ -101,6 +105,10 @@ def get_detail_url(url, selected_proxies):
             except TypeError as e:
                 print(e)
                 end = True
+            if page > 2:
+                end = True
+            else:
+                pass
             html_detail_urls = soup.select(detail_url_locators)
             url_locator = "div.cy5jw6o.dir.dir-ltr > a"
             for i in html_detail_urls:
@@ -119,13 +127,13 @@ def get_datas(urls, selected_proxies):
     print('Collecting datas...')
     datas = list()
     name_locator = 'h1._fecoyn4'
-    profile_locator = 'div.h1144bf3.dir.dir-ltr>div.a'
+    profile_locator = 'div.h1144bf3.dir.dir-ltr > div > a'
     # website_locator = 'div.d1isfkwk.dir.dir-ltr>span.ll4r2nl.dir.dir-ltr'
     selected_proxy = random.choice(selected_proxies)
     for counter, url in enumerate(urls):
         parsed_url = url.split('/')
         url_schema = f"{parsed_url[0]}/{parsed_url[1]}/{parsed_url[2]}"
-        if counter % 10:
+        if counter % 10 == 0:
             selected_proxy = random.choice(selected_proxies)
             print(f"Change proxy to {selected_proxy}")
         else:
@@ -145,10 +153,12 @@ def get_datas(urls, selected_proxies):
                 driver.delete_all_cookies()
                 driver.fullscreen_window()
                 driver.get(url)
-                WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR,name_locator)))
-                status_code = ec.presence_of_element_located((By.CSS_SELECTOR,name_locator))
+                WebDriverWait(driver, 20).until(ec.presence_of_element_located((By.CSS_SELECTOR,profile_locator)))
+                status_code = ec.presence_of_element_located((By.CSS_SELECTOR,profile_locator))
+                # print(driver.find_element(By.TAG_NAME, 'body').get_attribute('outerHTML'))
                 print(status_code)
             except Exception as e:
+                driver.quit()
                 print(e)
                 selected_proxy = random.choice(selected_proxies)
                 print(f"Change proxy to {selected_proxy}")
@@ -160,15 +170,16 @@ def get_datas(urls, selected_proxies):
             data['link'] = driver.current_url
             data['name'] = driver.find_element(By.CSS_SELECTOR, name_locator).text
             try:
-                profile_url = f"{url_schema}{driver.find_element(profile_locator).get_attribute('href')}"
-                print(profile_url)
+                profile_url = driver.find_element(By.CSS_SELECTOR,profile_locator).get_attribute('href')
+                print(f'profile url is {profile_url}')
+
                 data['instagram'] = get_instagram(url=profile_url, selected_proxy=selected_proxy)
             except Exception as e:
                 print(e)
                 data['instagram'] = None
 
             # data['website'] = soup.select_one(website_locator)
-
+            driver.quit()
             datas.append(data.copy())
 
         else:
@@ -210,15 +221,24 @@ def get_instagram(url,selected_proxy):
         "https": f"http://{selected_proxy}"
     }
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0',
+        'Host': 'www.airbnb.com',
+        'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / avif, image / webp, * / *;q = 0.8',
+        'Accept - Language': 'en - US, en;q = 0.5',
+        'Accept - Encoding': 'gzip, deflate, br',
+        'Connection': 'keep - alive'
     }
-    response = session.get(url, proxies=formated_proxy, headers=headers, timeout=(3, 27))
+    response = session.get(url, proxies=formated_proxy, headers=headers, timeout=(5, 27))
+    time.sleep(5)
     session.close()
     soup = BeautifulSoup(response.text, 'html.parser')
-    job_locator = 'div._o7dyhr>section>div:nth-child(4)>section>div:nth-child(2)>div:nth-child(3)>span._1ax9t0a'
-    job = soup.select_one(job_locator).text
-    instagram = job.split('@')[-1].split()[0]
-    return instagram
+    # print(soup.prettify())
+    job_locator = 'div._o7dyhr>section>div:nth-child(4)>section>div:nth-child(2)>div:nth-child(1)>span._1ax9t0a'
+    # job_locator = 'div._o7dyhr'
+    job = soup.select_one(job_locator)
+    print(job.html)
+    # instagram = job.split('@')[-1].split()[0]
+    # return instagram
 
 def to_csv(datas=None, filepath=None):
     print('Creating file...')
@@ -241,9 +261,16 @@ if __name__ == '__main__':
     save_path = "C:/project/airbnbscraper/result.csv"
     # proxies = get_proxy()
     # selected_proxies = choose_proxy(proxies)
-    selected_proxies = ['8.219.97.248:80','20.210.26.214:3128','168.138.33.70:8080', '203.154.91.28:8080','45.91.133.137:8080', '109.207.76.37:8080', '66.42.53.233:8000', '178.33.198.181:3128']
+    # print(selected_proxies)
+    selected_proxies = ['51.79.50.22:9300', '115.144.101.200:10000', '118.27.113.167:8080', '115.68.221.147:80', '103.248.197.12:3125', '168.138.33.70:8080', '44.230.152.143:80', '139.59.255.37:443']
     # detail_urls = get_detail_url(url, selected_proxies=selected_proxies)
     # print(detail_urls)
-    detail_urls = ['https://www.airbnb.com//rooms/45249678?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000']
+    detail_urls = [
+        'https://www.airbnb.com//rooms/41953733?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000',
+        'https://www.airbnb.com//rooms/19157408?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000',
+        'https://www.airbnb.com//rooms/45249678?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000',
+        'https://www.airbnb.com//rooms/670806019201784094?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000',
+        'https://www.airbnb.com//rooms/648208678396952595?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000',
+        'https://www.airbnb.com//rooms/50257276?adults=1&check_in=2023-01-29&check_out=2023-01-30&previous_page_section_name=1000']
     datas = get_datas(detail_urls, selected_proxies=selected_proxies)
     to_csv(datas, save_path)
